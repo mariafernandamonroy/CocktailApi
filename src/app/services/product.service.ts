ab747848cabafb26ef0758d13afa43e5b7cd9df9
María Fernanda Monroy <maria.monroy@sofka.com.co>
2024-03-17 21:49:59 -0500

    construction of get service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  API_URL: string = 'https://pokeapi.co/api/v2/pokemon/ditto';
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts() : Observable<any> {
+    return this.httpClient.get(this.API_URL).pipe(res=>res);
+  }
 }
