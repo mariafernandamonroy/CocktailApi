ffc354129f225a7a8f46746e6d1856f3db4c423c
María Fernanda Monroy <maria.monroy@sofka.com.co>
2024-03-17 21:48:01 -0500

    proyect and product component creation

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+
+}
